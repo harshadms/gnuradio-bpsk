908fad62e44827a7c9d4a31509bd94dfa62d3fb1
Harshad <harshad.sathaye@inf.ethz.ch>
2023-07-26 12:45:58 +0200

    simple zmq script msg input

--- zeromq_input.py
+++ zeromq_input.py
@@ -9,125 +9,30 @@
 # Author: harshad
 # GNU Radio version: v3.11.0.0git-489-g76831245
 
-from packaging.version import Version as StrictVersion
-from PyQt5 import Qt
-from gnuradio import qtgui
-from gnuradio import blocks, gr
-from gnuradio import gr
-from gnuradio.filter import firdes
-from gnuradio.fft import window
-import sys
-import signal
-from PyQt5 import Qt
-from argparse import ArgumentParser
-from gnuradio.eng_arg import eng_float, intx
-from gnuradio import eng_notation
-from gnuradio import zeromq
-import sip
+# zmq_PUSH_PULL_server.py
 
+import pmt
+import zmq
+import struct
+import numpy as np
 
+_debug = 0          # set to zero to turn off diagnostics
 
-class zeromq_input(gr.top_block, Qt.QWidget):
+# create a PUSH socket
+PROTOCOL = "tcp://"
+SERVER = "127.0.0.1"          # localhost
+PUSH_PORT = ":1234"
+PUSH_ADDR = PROTOCOL + SERVER + PUSH_PORT
 
-    def __init__(self):
-        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Not titled yet")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except BaseException as exc:
-            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
+push_context = zmq.Context()
+push_sock = push_context.socket (zmq.PUSH)
+push_sock.bind(PUSH_ADDR)
 
-        self.settings = Qt.QSettings("GNU Radio", "zeromq_input")
+while 1:
+    ch = input("Enter message: ")
 
-        try:
-            if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-                self.restoreGeometry(self.settings.value("geometry").toByteArray())
-            else:
-                self.restoreGeometry(self.settings.value("geometry"))
-        except BaseException as exc:
-            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+    if ch == 'q':
+        break
 
-        ##################################################
-        # Variables
-        ##################################################
-        self.samp_rate = samp_rate = 32000
-
-        ##################################################
-        # Blocks
-        ##################################################
-
-        self.zeromq_push_msg_sink_0 = zeromq.push_msg_sink('tcp://127.0.0.1:1234', 100, True)
-        self.zeromq_pull_msg_source_0 = zeromq.pull_msg_source('tcp://127.0.0.1:1234', 100, False)
-        self.qtgui_edit_box_msg_0 = qtgui.edit_box_msg(qtgui.STRING, 'Send', 'Input', False, True, '', None)
-        self._qtgui_edit_box_msg_0_win = sip.wrapinstance(self.qtgui_edit_box_msg_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_edit_box_msg_0_win)
-        self.blocks_message_debug_0 = blocks.message_debug(True, gr.log_levels.info)
-
-
-        ##################################################
-        # Connections
-        ##################################################
-        self.msg_connect((self.qtgui_edit_box_msg_0, 'msg'), (self.zeromq_push_msg_sink_0, 'in'))
-        self.msg_connect((self.zeromq_pull_msg_source_0, 'out'), (self.blocks_message_debug_0, 'print'))
-
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "zeromq_input")
-        self.settings.setValue("geometry", self.saveGeometry())
-        self.stop()
-        self.wait()
-
-        event.accept()
-
-    def get_samp_rate(self):
-        return self.samp_rate
-
-    def set_samp_rate(self, samp_rate):
-        self.samp_rate = samp_rate
-
-
-
-
-def main(top_block_cls=zeromq_input, options=None):
-
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
-    tb = top_block_cls()
-
-    tb.start()
-
-    tb.show()
-
-    def sig_handler(sig=None, frame=None):
-        tb.stop()
-        tb.wait()
-
-        Qt.QApplication.quit()
-
-    signal.signal(signal.SIGINT, sig_handler)
-    signal.signal(signal.SIGTERM, sig_handler)
-
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
-
-    qapp.exec_()
-
-if __name__ == '__main__':
-    main()
+    push_sock.send(pmt.serialize_str(pmt.cons(pmt.make_dict(), pmt.pmt_to_python.numpy_to_uvector(np.array([ord(c) for c in ch], np.uint8)))))
+#push_sock.send(pmt.serialize_str(pmt.to_pmt(tab)))
---
+++ zeromq_rx.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# Author: harshad
+# GNU Radio version: v3.11.0.0git-489-g76831245
+
+from gnuradio import blocks, gr
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import zeromq
+
+
+
+
+class zeromq_rx(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.zeromq_pull_msg_source_0 = zeromq.pull_msg_source('tcp://127.0.0.1:1234', 100, False)
+        self.blocks_message_debug_0 = blocks.message_debug(True, gr.log_levels.info)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.zeromq_pull_msg_source_0, 'out'), (self.blocks_message_debug_0, 'print'))
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
+
+
+
+def main(top_block_cls=zeromq_rx, options=None):
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
